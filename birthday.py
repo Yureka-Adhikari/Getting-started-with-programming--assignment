5abd488ca907f76663a8d806ee2564d7e777a911
Yureka Adhikari <adhikariyureka@gmail.com>
2024-11-08 08:52:49 +0545

    first commit

---
+++ birthday.py
@@ -0,0 +1,19 @@
+name1 = "Yureka"
+bday1 = "November 13"
+print(f"Name: {name1} \nBirthday: {bday1}")
+
+name2 = "Hima"
+bday2 = "November 7"
+print(f"\nName: {name2} \nBirthday: {bday2}")
+
+name3 = "Xenial"
+bday3 = "April 11"
+print(f"\nName: {name3} \nBirthday: {bday3}")
+
+name4 = "Subhaya"
+bday4 = "September 8"
+print(f"\nName: {name4} \nBirthday: {bday4}")
+
+name5 = "Aadhyashree"
+bday5 = "April 27"
+print(f"\nName: {name5} \nBirthday: {bday5}")
